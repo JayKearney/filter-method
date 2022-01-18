67e936768999e9c68f43ee3ec71d522522705c02
JayKearney <indi.mh@gmail.com>
2022-01-18 14:24:05 +0000

    add more complex filter functions

--- filter.js
+++ filter.js
@@ -62,10 +62,7 @@ const students = [
   },
 ];
 
-const hasStrongSkills = student => {
-  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
-  return strongSkills.length > 0;
-};
-
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
 const candidates = students.filter(hasStrongSkills);
 console.log(candidates);
